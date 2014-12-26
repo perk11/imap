f2cd3764fd242a980cd711779d5e4731339b75de
Pereyaslov Konstantin <perk11@perk11.info>
2014-12-26 08:18:41 +0300

    Using mb_convert_encoding way to encode/decode mailbox names

--- src/Ddeboer/Imap/Connection.php
+++ src/Ddeboer/Imap/Connection.php
@@ -63,7 +63,7 @@ class Connection
             throw new MailboxDoesNotExistException($name);
         }
 
-        return new Mailbox($this->server . \imap_utf7_encode($name), $this);
+        return new Mailbox($this->server . mb_convert_encoding($name, "UTF7-IMAP", "UTF-8"), $this);
     }
 
     /**
@@ -134,7 +134,7 @@ class Connection
         if (null === $this->mailboxNames) {
             $mailboxes = \imap_getmailboxes($this->resource, $this->server, '*');
             foreach ($mailboxes as $mailbox) {
-                $this->mailboxNames[] = \imap_utf7_decode(str_replace($this->server, '', $mailbox->name));
+                $this->mailboxNames[] = \mb_convert_encoding(str_replace($this->server, '', $mailbox->name), "UTF-8", "UTF7-IMAP");
             }
         }
 
